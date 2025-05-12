323803a97db2693b12041d6f4e4918a39afbde5e
Andreas Olsson <andreas.o.olsson@outlook.com>
2025-05-12 13:28:16 +0200

    Complete fastFibo2

--- assignments/A2N-memoization/src/Memoization.hs
+++ assignments/A2N-memoization/src/Memoization.hs
@@ -2,14 +2,16 @@ module Memoization where
 
 -- This is an assignment where we try to memoize functions
 -- To speed up computations.
-
-import Data.Maybe (fromJust)
+import           Data.Maybe (fromJust)
 
 -- The problem
 -- If we write a recursive function, it may be slow
 fibo :: Int -> Int
 {- TO BE WRITTEN -}
-fibo = undefined
+fibo n
+  | n == 0 = 0
+  | n == 1 = 1
+  | otherwise = fibo (n - 1) + fibo (n - 2)
 
 -- In GHCI, do,
 -- > :set +s
@@ -22,8 +24,8 @@ runSlow = fibo 30
 -- We can make it faster, by creating a cache
 -- This takes a function and creates an infinite list of the results
 -- (This works because Haskell doesn't evaluate things before we need them)
-mkCache :: (Num a , Enum a ) => (a -> b) -> [b]
-mkCache f = map f [0..]
+mkCache :: (Num a, Enum a) => (a -> b) -> [b]
+mkCache f = map f [0 ..]
 
 -- And we make a function to look for a value in a cache
 evalCache :: [a] -> Int -> a
@@ -43,7 +45,7 @@ fastFibo1 0 = 0
 fastFibo1 1 = 1
 fastFibo1 n =
   let fib = evalCache fiboCache
-  in fib (n-1) + fib (n-2)
+   in fib (n - 1) + fib (n - 2)
 
 -- We can see the cache growing by doing, in GHCI
 -- > :print fiboCache -- Prints just 'fiboCache = (_t1::[Integer])' since the list isn't evaluated yet
@@ -57,36 +59,37 @@ runFast = fastFibo1 30
 -- We pass the function and the domain of the keys as an argument
 listCache :: [a] -> (a -> b) -> [(a, b)]
 {- TO BE WRITTEN -}
-listCache domain f = undefined
+listCache domain f = map (\x -> (x, f x)) domain
 
 -- We create a function which looks up the
 -- result in the cache
 -- and use fromJust to get an error if the cache misses.
 listLookup :: Eq a => [(a, b)] -> a -> b
 {- TO BE WRITTEN -}
-listLookup cache value = undefined
+listLookup cache value = fromJust (lookup value cache)
 
 -- Create the cache for all integers...
 -- We use a 'fast fibonacci function' even if we haven't defined it yet!
 fibCache :: [(Int, Int)]
 {- TO BE WRITTEN -}
-fibCache = undefined
+fibCache = listCache [0 ..] fastFibo2
 
 -- And the fast function looks in the cache!
 fastFibo2 :: Int -> Int
 {- TO BE WRITTEN -}
-fastFibo2 n = undefined
+fastFibo2 0 = 0
+fastFibo2 1 = 1
+fastFibo2 n =
+  let fib = listLookup fibCache
+   in fib (n - 1) + fib (n - 2)
 
--- Pause:
 -- We make the solution in two parts:
 -- 1. We have a cache that uses a 'fast function' we don't have yet (but we know it's type!)
 -- 2. We have a fast function that looks for the result in the cache
 -- This wouldn't work in other languages, but because of lazy evaluation, it works, magic!
-
 -- Using criterion, we can see that the fastFib function is a little slower than fasterFibo.
 -- by using "stack bench --benchmark-arguments="--output bench.html"
 -- And looking at the plots.
-
 -- Now, for something cool
 -- What if we make a function that creates the cache, and immediately looks in it?
 memoizeWithList :: Eq a => [a] -> (a -> b) -> (a -> b)
@@ -95,18 +98,16 @@ memoizeWithList domain = listLookup . listCache domain
 -- Maybe we can use it to memoize the old fibo function?
 testMemoize :: Int -> Int
 testMemoize n =
-  let fibo2 = memoizeWithList [0..] fibo
-  in fibo2 n
+  let fibo2 = memoizeWithList [0 ..] fibo
+   in fibo2 n
 
 -- It doesn't work... because the old fibo function calls itself
 -- So even if we use memoize, the recursive calls don't use the cache...
-
 -- To fix this, we need open recursion.
 -- It looks tricky, but it's simple
 -- 1. Look at the recursive function
 -- 2. Replace the function calls to calls to another function
 -- 3. And his 'other function' is a function f, passed as a parameter.
-
 -- Here is Fibonacci in open recursion
 -- This function can do other things than compute fibonacci, but
 -- It isn't really recursive anymore
@@ -124,9 +125,7 @@ fastFibo3 = undefined
 -- The memoize function creates the cache and looks in it immediately
 -- And because of Lazy evaluation, we get a function that takes a slow
 -- version, and returns a fast version.
-
 -- LONGEST PALINDROMIC SUBSEQUENCE
-
 -- Utility
 dropLast :: [a] -> [a]
 dropLast l = take (length l - 1) l
@@ -136,7 +135,6 @@ lps :: String -> String
 lps s = undefined
 
 -- CACHES FOR LISTS OF THINGS
-
 -- If we want caches for lists, it's more complicated...
 -- Having a list of all possible lists will not work
 -- (Cause we meet an infinity of strings like 'a', 'aa', 'aaaaaa'
@@ -145,8 +143,9 @@ lps s = undefined
 -- A Trie node edge will store a value for the node, and edges
 -- to its children, where each label has type 'edge'
 -- 'node' and 'edge' can be any types.
-data Trie node edge = Trie node [(edge, Trie node edge)]
-  deriving Show
+data Trie node edge =
+  Trie node [(edge, Trie node edge)]
+  deriving (Show)
 
 -- First, looking for a list in a trie...
 trieLookup :: Eq e => Trie a e -> [e] -> a
@@ -156,9 +155,8 @@ trieLookup t l = undefined
 -- Get a subset of a trie, with limited depth
 -- (Provided: Useful for debugging)
 limitTrie :: Int -> Trie n e -> Trie n e
-limitTrie 0 (Trie v _) = Trie v []
-limitTrie n (Trie v edges) =
-  Trie v [(l, limitTrie (n-1) t) | (l, t) <- edges]
+limitTrie 0 (Trie v _)     = Trie v []
+limitTrie n (Trie v edges) = Trie v [(l, limitTrie (n - 1) t) | (l, t) <- edges]
 
 -- Map a function over all values in the trie
 -- Edge labels stay the same.
@@ -192,11 +190,9 @@ edges domain currentNode = undefined
 -- (using the edges function)
 subtree :: [a] -> a -> [a] -> Trie [a] a
 {- TO BE WRITTEN -}
-subtree domain label parent =
-  undefined
+subtree domain label parent = undefined
 
 -- Important: the trie is infinite because edges calls subtree, and subtree calls edges.
-
 -- trieCache builds a cache for a function
 -- provided with a domain (for the list elements)
 trieCache :: [e] -> ([e] -> b) -> Trie b e
@@ -226,10 +222,9 @@ And then, you can do
 
 Prints the cache, and you should be able to see it has grown
 --}
-
 testTrie =
-  let cache = mapTrie reverse $ rootTrie ['a'..'z']
-  in trieLookup cache
+  let cache = mapTrie reverse $ rootTrie ['a' .. 'z']
+   in trieLookup cache
 
 -- We can test it on the LPS function
 -- Computes the "longest palyndromic subsequence"
@@ -237,13 +232,17 @@ testTrie =
 -- It's a palyndrome
 -- It contains characters of the input (but you can skip some)
 -- (Is slow)
-
 -- First, some test strings
 k1 = "writers"
+
 k2 = "vintner"
+
 l1 = "aferociousmonadatemyhamster"
+
 l2 = "functionalprogrammingrules"
+
 s1 = "bananrepubliksinvasionsarmestabsadjutant"
+
 s2 = "kontrabasfiolfodralmakarmästarlärling"
 
 openLPS :: (String -> String) -> (String -> String)
@@ -251,9 +250,7 @@ openLPS s = undefined -- look at 'lps' for inspiration
 
 -- Fast!
 fastLPS :: String -> String
-fastLPS s =
-  undefined
-
+fastLPS s = undefined
 -- So, what were the tricks?
 -- The first one is to build an infinite data-structure, to memoize the function
 -- And then your function looks in the cache for the answer!
