8fce9e7f23b806048e3095ae065f2549f8a7293c
Andreas Olsson <andreas.o.olsson@outlook.com>
2025-05-22 16:20:00 +0200

    Run tests to assert current state

--- assignments/A3N-parsing/clean_parser_assignment/TestParser.hs
+++ assignments/A3N-parsing/clean_parser_assignment/TestParser.hs
@@ -1,21 +1,28 @@
 {- Test for Parser.hs -}
+-- All tests seem correct!
 module TestParser where
 
-import Prelude hiding (return, fail)
-import Parser
+import           Parser
+import           Prelude hiding (fail, return)
 
-l1 = letter "abc"   {- Just('a',"bc") -}
-l2 = letter "123"   {- Nothing -}
-l3 = letter ""      {- Nothing -}
+l1 = letter "abc" {- Just('a',"bc") -}
 
-w1 = spaces "abc"  {- Just("","abc") -}
-w2 = spaces "  \t abc"  {- Just("  \t ","abc") -}
+l2 = letter "123" {- Nothing -}
 
-c1 = chars 2 "abc"          {-  Just ("ab","c")  -}
-c2 = chars 0 "ab"          {-  Just ("","ab")  -}
-c3 = chars 3 "ab"          {-  Nothing)  -}
+l3 = letter "" {- Nothing -}
 
-r1 = require ":=" ":= 1"     {- Just (":=","1") -}
-r2 = require "else" "then"     {- Program error: expecting else near then -}
+w1 = spaces "abc" {- Just("","abc") -}
+
+w2 = spaces "  \t abc" {- Just("  \t ","abc") -}
+
+c1 = chars 2 "abc" {-  Just ("ab","c")  -}
+
+c2 = chars 0 "ab" {-  Just ("","ab")  -}
+
+c3 = chars 3 "ab" {-  Nothing)  -}
+
+r1 = require ":=" ":= 1" {- Just (":=","1") -}
+
+r2 = require "else" "then" {- Program error: expecting else near then -}
 
 a4 = (accept "read" -# word) "read count" {-  Just ("count","") -}
